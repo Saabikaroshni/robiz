ba653113e78bb4e49fdd4fbcdc934e169e051113
Saabika Roshni S <saabikaroshni@gmail.com>
2025-06-04 13:03:06 +0530

    login

--- robiz/src/components/Login.jsx
+++ robiz/src/components/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <div>
+            <img src="/logo2.png" alt="Company Logo" />
+            <h2>Log In</h2>
+            <h4>Mobile Number</h4>
+            <input type="text"/>
+            <h4>Password</h4>
+            <input type="password"/>
+            <p>Forgot Password?</p>
+            <button>LOG IN</button>
+        </div>
+    </div>
   )
 }
 
